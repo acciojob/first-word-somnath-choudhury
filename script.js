4ad3584e7c730357a000415993a718301990ad0f
Somnath Choudhury <102171721+somnath-choudhury@users.noreply.github.com>
2025-08-13 23:44:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 function firstWord(s) {
   // your code here
+	let modified = s.trim()
+	let first = "";
+	if (modified.length === 0)
+		return s;
+	
+	for (let i = 0; i < modified.length; i++) {
+		let ch = modified[i];
+		if (ch !== ' '){
+			first += ch;
+		}
+		else{
+			break;
+		}
+	}
+	return first;
 }
-
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
